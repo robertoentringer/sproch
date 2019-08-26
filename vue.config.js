ad3e92f37ae8e1d3a4d0336bcdc5b47fa9118f7d
Roberto Entringer <r.entringer@gmail.com>
2019-08-26 18:01:07 +0200

    edit devServe config for *.local machine host works

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.alias.set("audio", path.resolve(__dirname + "/src/assets/audio"))
     config.resolve.alias.set("img", path.resolve(__dirname + "/src/assets/img"))
+  },
+  devServer: {
+    disableHostCheck: true
   }
 }
