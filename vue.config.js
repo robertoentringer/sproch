3d3ce9e038b486cd4adf54aa863f6c1358405717
Roberto Entringer <r.entringer@gmail.com>
2019-08-26 16:52:57 +0200

    vue.config  resolve alias assets `audio` and `img`

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+const path = require("path")
+
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.resolve.alias.set("audio", path.resolve(__dirname + "/src/assets/audio"))
+    config.resolve.alias.set("img", path.resolve(__dirname + "/src/assets/img"))
+  }
 }
