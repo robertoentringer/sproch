4fea8c19328595bbc99deb23de6eb609bafa159f
Roberto Entringer <r.entringer@gmail.com>
2019-08-26 16:55:03 +0200

    edit Home component to use Expression and Observer components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main class="home">
+    <Expression v-for="(expression, i) in expressions" :key="i" :expression="expression" />
+    <Observer v-if="loadMore" @enter="handleEnter">
+      <h2>Loading...</h2>
+    </Observer>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue"
+import expressions from "../data/expressions"
+import Expression from "@/components/Expression"
+import Observer from "@/components/Observer"
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Expression,
+    Observer
+  },
+  methods: {
+    handleEnter() {
+      if (this.loadMore) this.expressions.push(expressions[this.expressions.length])
+    }
+  },
+  computed: {
+    loadMore() {
+      return this.expressions.length < expressions.length
+    }
+  },
+  data() {
+    return {
+      observerVisible: true,
+      expressions: [expressions[0]]
+    }
   }
 }
 </script>
