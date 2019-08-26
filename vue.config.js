b8c80224cc657b7f48e9e6182dd15bd2640147f8
Roberto Entringer <r.entringer@gmail.com>
2019-08-26 18:50:48 +0200

    webpack config  optiization splitChunks `minSize` and `maxSize`

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ const path = require("path")
 module.exports = {
   productionSourceMap: false,
   chainWebpack: config => {
+    config.optimization.set("splitChunks", {
+      minSize: 10000,
+      maxSize: 10000
+    })
     config.resolve.alias.set("audio", path.resolve(__dirname + "/src/assets/audio"))
     config.resolve.alias.set("img", path.resolve(__dirname + "/src/assets/img"))
   },
