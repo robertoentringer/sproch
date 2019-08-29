bac812597462399a35d49f75081b8599cf2f2256
Roberto Entringer <r.entringer@gmail.com>
2019-08-29 17:02:27 +0200

    move data helper to utils folder and add sanitizeSlug

--- src/utils/dataHelper.js
+++ src/utils/dataHelper.js
@@ -1,27 +1,12 @@
 const fs = require("fs")
 const path = require("path")
 const expressions = require("./expressions.json")
-
-const slug = string =>
-  string
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/[-_,/;'":!?()]+/g, " ")
-    .split(" ")
-    .splice(0, 10)
-    .join(" ")
-    .trim()
-    .replace(/\s+/g, "-")
-    .toLowerCase()
-
-expressions.map((item, index) => {
-  item.id = index
-  item.slug = slug(item.title)
-})
-
+const slug = require("../utils/sanitizeSlug.js")
 const assetsFolder = path.resolve(__dirname, "./../assets")
 
 expressions.forEach((item, i) => {
+  item.id = i
+  item.slug = slug(item.title)
   item.img = item.img ? fs.readFileSync(assetsFolder + `/img/${item.img}`, { encoding: "base64" }) : ""
   item.audio = item.audio ? fs.readFileSync(assetsFolder + `/audio/${item.audio}`, { encoding: "base64" }) : ""
   fs.writeFileSync(__dirname + `/json/${i + 1}.json`, JSON.stringify(item, null, 2))
