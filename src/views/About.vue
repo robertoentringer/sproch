852f51b1200f0b5b2dc67bec0ab333bbfbcb1ca5
Roberto Entringer <r.entringer@gmail.com>
2019-08-29 18:49:55 +0200

    edit page about : update link to portail lux fix spaces slit titles

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="about">
     <h1>De Sproch vun der Woch</h1>
-    <h2>Le proverbe de la semaine - The weekly expression - Der Spruch der Woche</h2>
+    <h2>Le proverbe de la semaine</h2>
+    <h3>The weekly expression</h3>
+    <h3>Der Spruch der Woche</h3>
     <p>
       Les expressions reproduites sur ce site ont étés origilamente publiés sur le
       <a
@@ -16,7 +18,7 @@
     <p>
       <a
         title="Portail officiel du Grand-Duché de Luxembourg"
-        href="http://luxembourg.public.lu/fr/le-grand-duche-se-presente/langues/sproch-woch/index.html"
+        href="http://luxembourg.public.lu"
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -26,10 +28,25 @@
   </section>
 </template>
 
+<script>
+export default {
+  name: "About"
+}
+</script>
+
 <style lang="scss" scoped>
 .about {
+  h3 {
+    &:last-of-type {
+      margin-top: 0;
+    }
+    &:first-of-type {
+      margin-bottom: 0;
+    }
+  }
   img {
     background-color: $bg-light;
+    margin-top: 1rem;
     padding: 10px;
     border-radius: 2px;
   }
