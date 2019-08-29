fa91804447a316f456e7311b51b1a9de2c7630c4
Roberto Entringer <r.entringer@gmail.com>
2019-08-29 13:59:55 +0200

    add assets logo luxemgourg portail and edit page abour

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="about">
+    <h1>De Sproch vun der Woch</h1>
+    <h2>Le proverbe de la semaine - The weekly expression - Der Spruch der Woche</h2>
+    <p>
+      Les expressions reproduites sur ce site ont étés origilamente publiés sur le
+      <a
+        title="Portail officiel du Grand-Duché de Luxembourg"
+        href="http://luxembourg.public.lu"
+        target="_blank"
+        rel="noopener noreferrer"
+        >portail officiel du Grand-Duché de Luxembourg</a
+      >
+      et disponibilisées ici avec l'autorisation préable de son editor.
+    </p>
+    <p>
+      <a
+        title="Portail officiel du Grand-Duché de Luxembourg"
+        href="http://luxembourg.public.lu/fr/le-grand-duche-se-presente/langues/sproch-woch/index.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="../assets/luxembourg.png" alt="logo-portail-grand-duche-du-luxembourg" />
+      </a>
+    </p>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  img {
+    background-color: $bg-light;
+    padding: 10px;
+    border-radius: 2px;
+  }
+}
+</style>
