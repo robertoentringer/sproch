5890820b9ac1deeabf630f6030d936f0e42a939a
Roberto Entringer <r.entringer@gmail.com>
2019-08-29 03:28:46 +0200

    edit data helper

--- src/data/helper.js
+++ src/data/helper.js
@@ -1,11 +1,28 @@
 const fs = require("fs")
-//const path = require("path")
+const path = require("path")
 const expressions = require("./expressions.json")
 
-//const assetsFolder = path.resolve(__dirname, "./../assets")
+const slug = string =>
+  string
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[-_,/;'":!?()]+/g, " ")
+    .split(" ")
+    .splice(0, 10)
+    .join(" ")
+    .trim()
+    .replace(/\s+/g, "-")
+    .toLowerCase()
+
+expressions.map((item, index) => {
+  item.id = index
+  item.slug = slug(item.title)
+})
+
+const assetsFolder = path.resolve(__dirname, "./../assets")
 
 expressions.forEach((item, i) => {
-  //item.img = item.img ? fs.readFileSync(assetsFolder + `/img/${item.img}`, { encoding: "base64" }) : ""
-  //item.audio = item.audio ? fs.readFileSync(assetsFolder + `/audio/${item.audio}`, { encoding: "base64" }) : ""
+  item.img = item.img ? fs.readFileSync(assetsFolder + `/img/${item.img}`, { encoding: "base64" }) : ""
+  item.audio = item.audio ? fs.readFileSync(assetsFolder + `/audio/${item.audio}`, { encoding: "base64" }) : ""
   fs.writeFileSync(__dirname + `/json/${i + 1}.json`, JSON.stringify(item, null, 2))
 })
