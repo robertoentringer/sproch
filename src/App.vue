f4ceee637a4cb656545d81e5d3c8ceb4fb47b14d
Roberto Entringer <r.entringer@gmail.com>
2019-08-31 23:44:04 +0200

    add transition router-view

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div id="app" :class="pageClass">
     <NavBar />
     <main>
+      <transition name="fade-down" mode="out-in" appear>
+        <router-view />
+      </transition>
       <router-view />
     </main>
   </div>
@@ -37,6 +40,34 @@ export default {
         margin-top: 0;
       }
     }
+  }
+}
+.fade-down-enter-active {
+  animation-name: fadeInDown;
+  animation-duration: 0.5s;
+}
+.fade-down-leave-active {
+  animation-name: fadeOutDown;
+  animation-duration: 0.4s;
+}
+@keyframes fadeInDown {
+  from {
+    opacity: 0.05;
+    transform: translate3d(0, -25px, 0);
+  }
+  to {
+    opacity: 1;
+    transform: none;
+  }
+}
+@keyframes fadeOutDown {
+  from {
+    opacity: 1;
+    transform: none;
+  }
+  to {
+    opacity: 0.05;
+    transform: translate3d(0, -25px, 0);
   }
 }
 </style>
