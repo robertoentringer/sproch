2a22e7d66ede39277943f076b527808c5a49add2
Roberto Entringer <r.entringer@gmail.com>
2019-08-31 23:44:50 +0200

    Simplify Observer

--- src/components/Observer.vue
+++ src/components/Observer.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="tag">{{ text }}</component>
+  <div>Loading...</div>
 </template>
 
 <script>
@@ -7,30 +7,13 @@ import "intersection-observer"
 
 export default {
   abstract: true,
-  props: {
-    tag: {
-      default: "h2",
-      type: String
-    },
-    text: {
-      default: "Loading...",
-      type: String
-    }
-  },
+  data: () => ({ observer: null }),
   mounted() {
-    this.observer = new IntersectionObserver(this.callback)
-    this.$nextTick(() => this.observer.observe(this.$el))
+    this.observer = new IntersectionObserver(([entry]) => entry.isIntersecting && this.$emit("intersect"))
+    this.observer.observe(this.$el)
   },
   destroyed() {
     this.observer.disconnect()
-  },
-  methods: {
-    callback(entries) {
-      if (entries[0].isIntersecting) this.$emit("enter")
-    }
-  },
-  render() {
-    return this.$el
   }
 }
 </script>
