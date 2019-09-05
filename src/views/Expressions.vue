c413f3dfba3142069d599293eddce315f05a15bc
Roberto Entringer <r.entringer@gmail.com>
2019-09-05 03:29:51 +0200

    work in pagination...

--- src/views/Expressions.vue
+++ src/views/Expressions.vue
@@ -17,9 +17,9 @@ export default {
   components: { Expression },
   data() {
     return {
-      perPage: 2,
+      perPage: 10,
       expressions: [],
-      page: this.$route.query.page || 1,
+      page: parseInt(this.$route.query.page) || 1,
       observer: null
     }
   },
@@ -29,46 +29,37 @@ export default {
     },
     loaded() {
       return this.expressions.length
-    },
-    pages() {
-      return Math.ceil(this.total / this.perPage)
-    }
-  },
-  watch: {
-    $route: {
-      immediate: true,
-      handler() {
-        console.log("watch $route")
-      }
     }
   },
   created() {
     this.expressions.push(...expressions.slice(this.loaded, this.loaded + this.perPage * this.page))
   },
   mounted() {
-    this.observer = new IntersectionObserver(this.handleIntersect)
-    this.observeLast()
+    this.observer = new IntersectionObserver(this.intersect)
+    this.observe()
   },
   destroyed() {
-    this.observer.disconnect()
+    this.disconnect()
   },
   methods: {
-    observeLast() {
+    disconnect() {
+      this.observer.disconnect()
+    },
+    observe() {
       this.$nextTick(() => {
         this.observer.observe(this.$refs.items[this.loaded - 1].$el)
       })
     },
-    handleIntersect(entries) {
-      entries.forEach(({ target, isIntersecting }) => {
-        if (isIntersecting) {
-          this.observer.unobserve(target)
-          if (this.loaded < this.total) {
-            this.expressions.push(...expressions.slice(this.loaded, this.loaded + this.perPage))
-            this.observeLast()
-            this.$router.push({ query: { page: ++this.page } })
-          } else this.observer.disconnect()
-        }
-      })
+    intersect([{ target, isIntersecting }]) {
+      if (isIntersecting) {
+        this.observer.unobserve(target)
+        if (this.loaded < this.total) {
+          this.page++
+          this.expressions.push(...expressions.slice(this.loaded, this.loaded + this.perPage))
+          this.observe()
+          this.$router.push({ query: { page: this.page } })
+        } else this.disconnect()
+      }
     }
   }
 }
