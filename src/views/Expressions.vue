23440d533ae40f6c0b12b9d0b5e4c58755b2fc21
Roberto Entringer <r.entringer@gmail.com>
2019-11-28 12:48:00 +0100

    clear code and loading expressions on demand works

--- src/views/Expressions.vue
+++ src/views/Expressions.vue
@@ -1,21 +1,19 @@
 <template>
-  <section>
+  <section class="expressions">
     <Expression
       v-for="(item, i) in expressions"
-      :id="item.slug"
       :key="i"
+      ref="items"
       :data-slug="item.slug"
-      :data-page="dataPage(i)"
       :expression="item"
     />
   </section>
 </template>
 
 <script>
+import "intersection-observer"
 import Expression from "@/components/Expression"
-import { getExpressionsFirsts } from "@/utils/getExpressions"
-
-const expressions = getExpressionsFirsts(20)
+import expressions from "@/utils/getExpressions"
 
 export default {
   name: "Expressions",
@@ -25,9 +23,9 @@ export default {
   components: { Expression },
   data() {
     return {
-      perPage: 4,
+      perPage: 10,
       expressions: [],
-      page: parseInt(this.$route.query.page) || 1,
+      page: this.$route.query.page || 1,
       observer: null
     }
   },
@@ -43,50 +41,29 @@ export default {
     this.expressions.push(...expressions.slice(this.loaded, this.loaded + this.perPage * this.page))
   },
   mounted() {
-    this.$nextTick(() => {
-      //this.$refs.items[7].$el.scrollIntoView()
-      const el = document.getElementById("keiseker")
-      el.style.border = "10px solid red"
-      el.scrollIntoView({
-        behavior: "smooth"
-      })
-      //el.parentNode.removeChild(el)
-      console.log(el)
-      /* window.scrollTo({
-        top: this.$refs.items[7].$el.getBoundingClientRect().top + window.pageYOffset
-      }) */
-    })
-    //const page = this.$route.query.page
-    //this.observer = new IntersectionObserver(this.intersect)
-    //this.$refs.items[0].$el.scrollIntoView()
-    //this.$refs.items.forEach(({ $el }) => {
-    //console.log($el.dataset.page)
-    //if ($el.dataset.page === page) $el.scrollIntoView()
-    //this.observer.observe($el)
-    //})
-    //this.intersectionObserverEntries = this.observer.takeRecords()
-    //if (this.$route.query.page) this.$router.push({ hash: `#page-${this.$route.query.page}` })
-  } /* ,
+    this.observer = new IntersectionObserver(this.handleIntersect)
+    this.observeLast()
+  },
   destroyed() {
     this.observer.disconnect()
-  }, */,
+  },
   methods: {
-    dataPage(i) {
-      return ++i % this.perPage === 0 ? Math.ceil(i / this.perPage + 1) : null
-    },
-    conditionalRef(i) {
-      return this.dataPage(i) ? "items" : null
-    } /* ,
-    intersect(entries, observer) {
-      entries.forEach(({ target, isIntersecting }) => {
-        //console.log(target.getBoundingClientRect().top + window.pageYOffset)
-        const slug = target.dataset.slug
-        const pageData = parseInt(target.dataset.page)
-        const pageUrl = parseInt(this.$route.query.page)
-        //console.log(slug, ", data:", pageData, ", url:", pageUrl, ", intersection:", isIntersecting)
-        //if (isIntersecting && pageUrl !== pageData) this.$router.push({ query: { page: pageData } })
+    observeLast() {
+      this.$nextTick(() => {
+        this.observer.observe(this.$refs.items[this.loaded - 1].$el)
       })
-    } */
+    },
+    handleIntersect(entries) {
+      entries.forEach(({ target, isIntersecting }) => {
+        if (isIntersecting) {
+          this.observer.unobserve(target)
+          if (this.loaded < this.total) {
+            this.expressions.push(...expressions.slice(this.loaded, this.loaded + this.perPage))
+            this.observeLast()
+          } else this.observer.disconnect()
+        }
+      })
+    }
   }
 }
 </script>
