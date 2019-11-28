15574133de17eb02a79c37e7db90bf6d8c437544
Roberto Entringer <r.entringer@gmail.com>
2019-11-28 13:00:30 +0100

    add suffix `Sproch.lu` for title

--- src/mixins/seo.js
+++ src/mixins/seo.js
@@ -7,7 +7,7 @@ export default {
   beforeRouteEnter(to, from, next) {
     next(vm => {
       const title = getValue("title", vm)
-      if (title) document.title = title
+      if (title) document.title = title.concat(" — Sproch.lu")
       const description = getValue("description", vm)
       if (description)
         document.head.querySelector("[name=Description]").setAttribute("content", description)
